07fe2a334bc1d58c6752bf5b49ad763d85df9cc8
Andrew Moses <andrewmosesdrive@gmail.com>
2021-05-09 21:57:13 -0700

    updated newestBook image sizing; Changed newestBook card text

--- src/components/newestBook.js
+++ src/components/newestBook.js
@@ -3,35 +3,45 @@ import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
-import Typography from '@material-ui/core/Typography'
-import img from '../images/sadderSongsImg.png'
+import Typography from '@material-ui/core/Typography';
+import img from '../images/sadderSongsImg.png';
+import { makeStyles } from '@material-ui/core/styles';
 
 const cardStyle = {
-    maxWidth: '345px'
+    maxWidth: '100px'
 }
 
+const useStyles = makeStyles({
+    root: {
+        maxWidth: 345,
+    },
+    media: {
+        height: 140,
+    },
+});
+
 export default function NewestBook() {
-  return (
-      <Card className={cardStyle.maxWidth}>
-      <CardActionArea>
-        <CardMedia
-          
-          component='img'
-          height="100"
-          width="100"
-          image={img}
-          title="Sadder Songs Book Cover"
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="h2">
-            "Sadder Songs" out now!
+
+    const classes = useStyles();
+
+    return (
+        <Card className={classes.root}>
+            <CardActionArea>
+                <div>
+                    <img src={`${img}`} alt="Sadder Songs Book Cover"></img>
+                </div>
+
+                <CardContent>
+
+                    <Typography gutterBottom variant="h5" component="h2">
+                        "Sadder Songs" out now!
           </Typography>
-          <Typography variant="body2" color="textSecondary" component="p">
-            The second book in the anthology series of poetry is now available through Amazon
+                    <Typography variant="body2" color="textSecondary" component="p">
+                        The second book of poetry in the series is now available through Amazon. Click on this tile to check it out on Amazon!
           </Typography>
-        </CardContent>
-      </CardActionArea>
-      
-    </Card>
-  );
+                </CardContent>
+            </CardActionArea>
+
+        </Card>
+    );
 }
